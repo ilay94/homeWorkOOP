0c7fa0a839e2b51d0c08cc3b2aaf8974f5c90185
ilay94 <tibaldshenk@gmail.com>
2024-11-18 23:10:25 +0300

    Обновлен README.md

--- src/lawngrass.py
+++ src/lawngrass.py
@@ -6,7 +6,6 @@ class LawnGrass(Product):
 
     country: str
     germination_period: str
-    color: str
 
     def __init__(
         self,
@@ -23,5 +22,5 @@ class LawnGrass(Product):
             raise TypeError("Строна производитель травы газонной должна быть строкой")
         self.country = country
         if type(germination_period) is not str:
-            raise TypeError("Период проростания травы газонной должен быть строкой")
+            raise TypeError("Срок прорастания травы газонной должен быть строкой")
         self.germination_period = germination_period
